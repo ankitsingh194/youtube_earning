766d8f17882cf80318a087ddbcbfe10ed0cc7119
ankitsingh194 <ankit1602121007@gmail.com>
2023-10-25 14:26:08 +0100

    first commit

---
+++ src/ytvideo.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API = "AIzaSyDbcVA6BvbaMK4UllEiQt9jWkQihTgZH1o"
+const Part = "snippet"
+
+export default axios.create({
+        baseURL: `https://www.googleapis.com/youtube/v3`,
+        params:{
+            key: API,
+        
+        }
+});
